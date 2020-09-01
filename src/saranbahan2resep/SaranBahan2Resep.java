c9dbd81f87be8b42c547f59cb05b69e6f2e81005
Elia Sari <Elia Sari@LAPTOP-S3GTQRE7>
2020-09-01 22:16:14 +0700

    Tambah Input Bahan 1

--- src/saranbahan2resep/SaranBahan2Resep.java
+++ src/saranbahan2resep/SaranBahan2Resep.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package saranbahan2resep;
+
+import java.util.Scanner;
 
 /**
  *
@@ -20,6 +17,14 @@ public class SaranBahan2Resep {
         System.out.println("Identitas : "+ identitas);
         
         System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+        
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("Bahan Pertama : ");
+        System.out.println("1. Pisang");
+        System.out.println("2. Telur");
+        System.out.println("Masukkan no pilihan Anda");
+        int bahan1 = scanner.nextInt();
     }
     
 }
